be925d003f00bcd85cf76c3305621de27287c9f1
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-10-12 12:21:53 -0700

    "updated release date field for date field instead of datetime field "

--- youtube_music_library/music/models.py
+++ youtube_music_library/music/models.py
@@ -7,4 +7,4 @@ class Song(models.Model):
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
